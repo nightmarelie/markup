016735a55be382f17837d7321c4b06ae2548c3e9
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-01-10 23:43:59 -0600

    feat: add navbar

---
+++ navbar/index.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleBtn.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
